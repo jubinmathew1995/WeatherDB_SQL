7e2767025d7ccb0a9fbd9d2d09801572d2c4c27d
Jubin Mathew <jubinmathew1995@gmail.com>
2016-11-18 10:08:43 +0530

    added mysql commands for the database

--- xml2DB.py
+++ xml2DB.py
@@ -1,40 +1,37 @@
 from bs4 import BeautifulSoup
 
-file = open("monday.xml")
+file = open("/home/jubin/WeatherDB_SQL/raw data/bangalore.xml")
 soup = BeautifulSoup(file,"xml")
 
 for x in soup.find_all('current'):
     city_id=x.city['id']
     city_name = x.city['name']
     country_code = x.country.string
-    #print country_code
-    #print(id+" "+name+"\n")
 
     rise = x.sun['rise']
     set = x.sun['set']
-    #print(rise +"\t"+set)
+    # print(rise +"\t"+set)
 
     recordtime = x.lastupdate['value']
-    #print recordtime
+    # print (recordtime)
 
     humidity_value = x.humidity['value']
     humidity_unit = x.humidity['unit']
-    #print humidity_value+humidity_unit
+    print (humidity_value+humidity_unit)
 
 
     pressure_value = x.pressure['value']
     pressure_unit = x.pressure['unit']
-    #print pressure_value+pressure_unit
+    print (pressure_value+pressure_unit)
 
     if(x.temperature['unit']=='kelvin'):
         temp_k =x.temperature['value']
-        #print temp_k
+        print (temp_k)
 
     wind = x.wind
     w_speed = wind.speed['value']
     w_type = wind.speed['name']
     w_direction = wind.direction['code']
     w_degree = wind.direction['value']
-    #print w_speed+w_type+w_degree+w_direction
-
-
+    print (w_speed+w_type+w_degree+w_direction)
+    print "\n"
