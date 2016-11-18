5c7e9b30e533b3ee86244cf110b78640a1c399de
Jubin Mathew <jubinmathew1995@gmail.com>
2016-11-18 10:49:06 +0530

    Added all the create table commands

--- xml2DB.py
+++ xml2DB.py
@@ -17,21 +17,21 @@ for x in soup.find_all('current'):
 
     humidity_value = x.humidity['value']
     humidity_unit = x.humidity['unit']
-    print (humidity_value+humidity_unit)
+    #print (humidity_value+humidity_unit)
 
 
     pressure_value = x.pressure['value']
     pressure_unit = x.pressure['unit']
-    print (pressure_value+pressure_unit)
+    #print (pressure_value+pressure_unit)
 
     if(x.temperature['unit']=='kelvin'):
         temp_k =x.temperature['value']
-        print (temp_k)
+        #print (temp_k)
 
     wind = x.wind
     w_speed = wind.speed['value']
     w_type = wind.speed['name']
     w_direction = wind.direction['code']
     w_degree = wind.direction['value']
-    print (w_speed+w_type+w_degree+w_direction)
-    print "\n"
+    #print (w_speed+w_type+w_degree+w_direction)
+    #print "\n"
